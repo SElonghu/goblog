58cd8f78146968fc5e61647b161db158701a6ed7
SElonghu <a1822308945@163.com>
2023-04-04 23:31:55 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>hello, 这是一个goblog项目！</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>hello, 这是一个goblog项目！</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客用以记录编程笔记，如有反馈或建议，请联系"+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
+	}
+
 }
 func main() {
 	http.HandleFunc("/", handleFunc)
