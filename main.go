4a6a222ff2fbf1f00c3449c92a7666f31625b471
SElonghu <a1822308945@163.com>
2023-04-05 01:37:00 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -5,19 +5,24 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>hello, 欢迎来到goblog项目！</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客用以记录编程笔记，如有反馈或建议，请联系"+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
 	}
 
 }
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客用以记录编程笔记，如有反馈或建议，请联系"+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
 func main() {
-	http.HandleFunc("/", handleFunc)
-	http.ListenAndServe("127.0.0.1:3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe("127.0.0.1:3000", router)
 }
